999c4f504242cb3155d565418276444860965f40
Mariel Salcedo <mjjsalcedo@yahoo.com>
2017-07-11 16:12:55 -1000

    gender

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -76,7 +76,11 @@ var club_name = "Fight Club";
  *   male    => "male"
  *   unknown => undefined
  */
-
+var gender = {
+  female: "female",
+  male: "male",
+  unknown: undefined,
+};
 
 /* Step 10
  *
