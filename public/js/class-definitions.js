bb4773838dabb93b37ef06ca718463a31fc64bb8
Mariel Salcedo <mjjsalcedo@yahoo.com>
2017-07-12 14:48:19 -1000

    george + nemo

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1105,8 +1105,10 @@
 
 
 // Create 2 different species of animals
-var george;
-var nemo;
+var george = new Animal("Monkey", "male");
+var nemo = new Animal("Fish", "male");
+
+
 
 // Create 2 different vehicles
 var civic;
